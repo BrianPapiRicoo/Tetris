c652c9bfe1ddec8be1148ce00d7d66ae921ea114
BrianPapiRicoo <A23310366@live.ceti.mx>
2024-11-21 17:35:59 -0600

    1er avance

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std; 
+using namespace sF; 
+
+int main()
+{
+    RenderWindow window(VideoMode(400,400),"Tetris");
+    window.setFramerateLimit(60);
+    while(window.isOpen())
+    {
+        Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == Event::Closed) window.close();
+        }
+        window.clear(Color(20,20,20));
+        window.display();
+    }
+    return 0; 
+}
